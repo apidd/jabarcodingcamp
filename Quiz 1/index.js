03a220f9f968610b778a49fefa526f962da964a3
Moh Rafid F S <rafidf2004@gmail.com>
2022-03-13 22:31:34 +0700

    Jawaban nomor 1

--- Quiz 1/index.js
+++ Quiz 1/index.js
@@ -1,18 +1,3 @@
-// function jumlah_kata(){
-//     var kalimat = kalimat_1
-//     var kata = kalimat.split(" ")
-//     return kata.length
-// }
-
-// var kalimat_1 ="Halo nama saya Muhammad Iqbal Mubarok"
-// var kalimat_2 ="Saya Iqbal"
-// var kalimat_3 ="Saya Muhammad Iqbal Mubarok"
-
-// console.log(jumlah_kata(kalimat_1))
-// console.log(jumlah_kata(kalimat_2))
-// console.log(jumlah_kata(kalimat_3))
-
-
 function jumlah_kata(kalimat){
     var kata = kalimat.split(" ")
     return kata.length
